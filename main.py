c6c0ec0d33655aacb5ae19013b71ad1d944d1617
Ivan Kylchik <cilcic.v22@gmail.com>
2019-11-10 23:01:41 +0300

    Rewrite code to be more correct with task
    The actual task was to answer the question:
    "Is this number combination correct and if it is show free cells"

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import reader
 import util
 
 
-def check_cell(board, row, column):
+def check_cell(board, row, column, known_free_cells=[]):
     width = len(board)
     height = len(board[0])
     cnf = CNF()
+
+    for mine in known_free_cells:
+        cnf.append([-util.coords_to_var(mine[0], mine[1], width)])
 
     for c in range(width + 2):
         cnf.append([-util.coords_to_var(0, c, width)])
@@ -23,7 +26,7 @@ def check_cell(board, row, column):
             if cell != '?':
                 cnf.append([-util.coords_to_var(x, y, width)])
                 neighbours = [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1],
-                              [x, y - 1], [x, y], [x, y + 1],
+                              [x, y - 1], [x, y + 1],
                               [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]]
                 neighbours_ids = util.neighbours_str_to_num(neighbours, width)
                 clauses = util.get_cell_constraints(int(cell), neighbours_ids)
@@ -34,12 +37,21 @@ def check_cell(board, row, column):
     g = Glucose4()
     g.append_formula(cnf.clauses)
 
-    if not g.solve():
-        print('row{} column{} has mine'.format(row, column))
+    #if not g.solve():
+    #    print('row{} column{} hasn\'t mine'.format(row, column))
+    return g.solve()
 
 
 board = reader.get_board_from_csv('data/board1.csv')
-for x in range(len(board)):
-    for y in range(len(board[x])):
-        if board[x][y] == '?':
-            check_cell(board, x + 1, y + 1)
+free_cells = []
+for i in range(len(board)):
+    for j in range(len(board[i])):
+        if board[i][j] == '?':
+            if not check_cell(board, i + 1, j + 1):
+                free_cells.append((i + 1, j + 1))
+
+if check_cell(board, -1, -1, free_cells):
+    print("Number combinations on the board is possible with free cells on: ")
+    print(free_cells)
+else:
+    print("Number combination on the board is impossible")
